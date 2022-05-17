226d7292210134220e0aa3f66c6732dfc3ff0a67
garg-kart <91060345+garg-kart@users.noreply.github.com>
2022-05-17 12:39:12 +0200

    Revert "Get error response body"

--- leanix/leanix_client.go
+++ leanix/leanix_client.go
@@ -72,7 +72,7 @@ func (leanix *LeanixClient) getAuthorizationHeader() (string, error) {
 		}
 
 		if resp.StatusCode != 200 {
-			return "", errors.New(fmt.Sprintf("Status code must be 200 but is %s", resp.Body))
+			return "", errors.New(fmt.Sprintf("Status code must be 200 but is %d", resp.StatusCode))
 		}
 
 		defer resp.Body.Close()
